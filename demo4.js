fd67aca12854cf21b98fb77800da29da7da744d4
DQing <15829258737@163.com>
2017-05-24 22:02:11 +0800

    add cure method

--- demo4.js
+++ demo4.js
@@ -11,6 +11,37 @@ var kittySchema = mongoose.Schema({
 });
 var Kitten = mongoose.model('kitten', kittySchema);
 var silence = new Kitten({name: "Silence"});
-silence.save(function (err) {
-    console.log('save success');
+// silence.save(function (err) {
+//     console.log('save success');
+// });
+//
+Kitten.find(function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+Kitten.find({name:'douqing'},function (err, result) {
+    if (err){
+        console.log(err);
+    }else {
+        console.log(result);
+    }
+});
+
+Kitten.remove({name:'douqing'},function (err, result) {
+    if (err){
+        console.log(err);
+    }else {
+        console.log(result);
+    }
+});
+
+Kitten.update({_id: '592573e222fe4833e74d5617'},{name:'heko'}, function (err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
 });
